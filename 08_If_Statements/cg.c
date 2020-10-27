47f8219ba1ebe51d4432c4d4f8e0b8b3d4c09b7b
liuyue <codelovect.@icloud.com>
2020-10-27 09:36:45 +0800

    modify cg.c compare_and_set()

--- 08_If_Statements/cg.c
+++ 08_If_Statements/cg.c
@@ -155,7 +155,7 @@ int cgcompare_and_set(int ASTop, int r1, int r2){
 
   fprintf(Outfile, "\tcmp\t%s, %s\n", reglist[r2], reglist[r1]);
   fprintf(Outfile, "\t%s\t%s\n", cmplist[ASTop - A_EQ], breglist[r2]);
-  fprintf(Outfile, "\tandq\t$255,%s\n", reglist[r2]);
+  fprintf(Outfile, "\tmovzbq\t%s, %s\n", breglist[r2], reglist[r2]);
   free_register(r1);
   return (r2);
 }
